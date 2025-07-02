a2f88502b8be51fa6a5d4df4540b7fdde97a3aca
TanmayPanja <tanmaypanja149@gmail.com>
2025-07-02 07:44:21 +0000

    Variables types and Declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const clientId = 14454
+let clientName = " Tanmay Panja "
+var clientEmail = "Tanmaypanja149@gmail.com"
+let clientState = "West Bengal"
+let clientCity
+
+console.log(clientState);
+console.table([clientId, clientName, clientEmail, clientState, clientCity]);
