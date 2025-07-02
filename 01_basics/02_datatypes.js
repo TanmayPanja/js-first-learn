0860704635c89f8e2e171a14602c4d23f10ea6c3
TanmayPanja <tanmaypanja149@gmail.com>
2025-07-02 08:28:05 +0000

    Datatypes for Java Script

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // use all javascript codes as a newer version
+// alert (3+3) // we are using node js not broswer
+
+let name= "Tanmay"
+let age = "30"
+let birthYear =1995
+let isLoggedIn = true
+let country;
+
+//number => 2 to the power 53
+//bigint => Where the values should be inserted as a large number ( e.g stock markert sites, facebook etc. )
+//string => ""
+//boolean => True/ False
+//undefined => Undefined
+//symbol = unique
+
+//
+
+console.log(typeof "Tanmay") // output is string
+console.log(typeof "age") // output is string though it is number as I have declared it within ""
+console.log(typeof birthYear) // output is number as I have declared it without ""
+console.log(typeof null) // output is object
+console.log(typeof country) // undefined
